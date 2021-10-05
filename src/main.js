3f964929c10734e45741e450276f71f806b0d8e6
Qwanjai Deelertpradit <d_khonjai@hotmail.com>
2021-10-06 00:11:58 +0700

    [:heavy_plus_sign:] Add axios

--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import store from "./store";
 import "./assets/tailwind.css";
 import WaveUI from "wave-ui/src/wave-ui";
 import "material-design-icons/iconfont/material-icons.css";
-
+import axios from "axios";
+import VueAxios from "vue-axios";
 // createApp(App).use(store).use(router).mount('#app')
 const app = createApp(App);
 app.use(store);
 app.use(router);
-// app.use(VueAxios, axios);
+app.use(VueAxios, axios);
 new WaveUI(app, { iconsLigature: "material-icons" });
 app.mount("#app");

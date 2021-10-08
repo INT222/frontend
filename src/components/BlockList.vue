7e780c29ccda07aa28b8acbc30a61731b08a59ff
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-10-08 22:29:49 +0700

    [ADD] component BlockList but Logic not yet finish

---
+++ src/components/BlockList.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="p-5">
+		<div class="bg-black">
+			<div class="w-full grid grid-cols-5 space-x-4 lg:max-w-full lg:flex">
+				<img
+					src="../src/assets/TheTomorrowWar.jpg"
+					class="h-48 col-span-2 lg:h-auto lg:w-48 flex-none bg-cover rounded-md lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+				/>
+				<div class="col-span-3 mt-3 ml-10">
+					<p class="text-white font-medium text-base mt-2 mb-4">The Tomorrow War</p>
+					<p class="text-gray-400 text-sm tracking-wide mb-3">July 2, 2021</p>
+					<p class="text-gray-400 text-sm tracking-wide mb-1">Action/Sci-fi ‧ 2h13m</p>
+                    <!-- import component --"FavButton"-- in this Line -->
+				</div>
+			</div>
+		</div>
+	</div>
+</template>

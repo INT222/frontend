2827553d02da8ab994f3a9e2b7451a2f3b2665b1
fah <supanan.c@mail.kmutt.ac.th>
2021-10-08 21:21:06 +0700

    [EDIT] edit BackButton.vue to work for all pages

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -2,8 +2,8 @@
     <div>
         <div class="mx-6 mt-4">
             <button class="inline-flex">
-                <!-- <img src="./assets/arrow_back.svg" /> -->
-                <p class="ml-4 text-xs tracking-wider leading-loose uppercase">back to home</p>
+                <w-icon xs color="black">material-icons arrow_back</w-icon>
+                <!-- <p class="mt-1 ml-4 text-xs tracking-wider leading-loose uppercase">back to home</p> -->
             </button>
         </div>
     </div>

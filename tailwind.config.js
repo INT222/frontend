83d8ed6a9c79c35e41a0ea2e569cfff346fbb017
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-10-09 15:10:00 +0700

    [ADD] CommentForm component only html&css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+				blackBlue: "#20232A",
+				deepBlue: "#3EB7D2",
+				grayBlue: "#292D37",
+				lightGray: "#323742",
+				midGray: "#2D3037",
+        deepBrownGray:"#484848"
+			},
+    },
   },
   variants: {
     extend: {},

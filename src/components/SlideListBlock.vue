3496a1a7095c67a84aa4a4fb54b8f0115ad7fc6a
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-10-11 13:33:22 +0700

    [ADD] SlideListBlock component

---
+++ src/components/SlideListBlock.vue
@@ -0,0 +1,17 @@
+<template>
+	<w-app>
+		<div class="inline-flex mx-auto space-x-2">
+			<div class="w-full">
+				<img src="../src/assets/BlackWidow.jpg" class="h-44 rounded-md" />
+				<p class="text-center text-sm font-medium leading-none ">Black Widow with The Adventure</p>
+				<w-rating xs color="amber-light1" />
+			</div>
+			<div class="w-full">
+				<img src="../src/assets/BlackWidow.jpg" class="h-44 rounded-md" />
+			</div>
+			<div class="w-full">
+				<img src="../src/assets/BlackWidow.jpg" class="h-44 rounded-md" />
+			</div>
+		</div>
+	</w-app>
+</template>

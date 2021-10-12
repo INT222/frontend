4ba1bf668145b51380764e5497edae6f8613a96f
fah <supanan.c@mail.kmutt.ac.th>
2021-10-12 22:00:27 +0700

    [EDIT] edit bookmark button

--- src/components/BookMarkButton.vue
+++ src/components/BookMarkButton.vue
@@ -1,16 +1,17 @@
 <template>
     <w-app>
-        <div>
-            <button
-					class="grey-light2 absolute left-20 top-0"
-					:class="{
-						'info-light1': changeColor == true,
-					}"
-					@click="changeColor = !changeColor"
-				>
-					<w-icon xs>material-icons bookmark</w-icon>
-				</button>
-        </div>
+        <div class="relative">
+				<!-- <img class="block h-44 md:h-60" src="../src/assets/LoveSimon.jpg"> -->
+			<button
+				class="grey-light2 absolute left-20 md:left-28 -top-2"
+				:class="{
+					'info-light1': changeColor == true,
+				}"
+				@click="changeColor = !changeColor"
+			>
+				<w-icon sm>material-icons bookmark</w-icon>
+			</button>
+			</div>
     </w-app>
 </template>
 

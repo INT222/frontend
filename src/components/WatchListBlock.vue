8beff6db194d9da8dca8ccc8a98f43b5f8a51fa0
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-10-13 13:42:28 +0700

    [ADD] WatchListBlock component

---
+++ src/components/WatchListBlock.vue
@@ -0,0 +1,21 @@
+<template>
+	<w-app>
+			<div class="w-full grid grid-cols-5 md:grid-cols-1 bg-black my-5">
+				<img
+					src="../src/assets/BlackWidow.jpg"
+					class="h-48 col-span-2 flex-none bg-cover rounded-md md:rounded-l md:object-center md:h-60 md:mx-auto"
+				/>
+				<div class="col-span-3 mt-3 mx-4">
+					<p
+						class="text-white font-medium text-base mt-2 mb-3 tracking-wide md:text-center md:mb-2"
+					>The Tomorrow War</p>
+					<p class="text-gray-400 text-sm tracking-wider mb-3 md:text-center md:mb-2">July 2, 2021</p>
+					<p class="text-gray-400 text-sm tracking-wider mb-3 md:text-center">Action/Sci-fi ‧ 2h13m</p>
+					<div class="inline-flex space-x-2">
+						<w-icon lg color="amber-light1">material-icons star</w-icon>
+						<span class="text-white text-md font-medium">4.5</span>
+					</div>
+				</div>
+			</div>
+	</w-app>
+</template>

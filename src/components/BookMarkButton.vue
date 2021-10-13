33842ce5696bad9e4a397bd4c7e3fcc099f66a2e
fah <supanan.c@mail.kmutt.ac.th>
2021-10-13 23:46:53 +0700

    [EDIT] edit Bookmark button

--- src/components/BookMarkButton.vue
+++ src/components/BookMarkButton.vue
@@ -1,26 +1,37 @@
 <template>
-    <w-app>
-        <div class="relative">
-				<!-- <img class="block h-44 md:h-60" src="../src/assets/LoveSimon.jpg"> -->
-			<button
-				class="grey-light2 absolute left-20 md:left-28 -top-2"
-				:class="{
-					'info-light1': changeColor == true,
-				}"
-				@click="changeColor = !changeColor"
-			>
-				<w-icon sm>material-icons bookmark</w-icon>
-			</button>
+	<w-app>
+		<div class="relative">
+			<!-- <img class="block h-44 md:h-60" src="../src/assets/LoveSimon.jpg"> -->
+			<div v-if="seen == false">
+				<button class="grey-light2 absolute left-20 md:left-28 -top-1" @click="toggleSeen">
+					<w-icon xl>material-icons bookmark_border</w-icon>
+				</button>
 			</div>
-    </w-app>
+			<div v-if="seen">
+				<button class="info-light1 absolute left-20 md:left-28 -top-1" @click="toggleSeen">
+					<w-icon xl>material-icons bookmark</w-icon>
+				</button>
+			</div>
+		</div>
+	</w-app>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			changeColor: false
+			button: [
+				{ text: 'Hide' }
+			],
+			seen: false
 		}
+	},
+	methods: {
+		toggleSeen() {
+			this.seen = !this.seen
+			this.button.push({ text: 'Show' })
+		}
+
 	}
 }
 </script>

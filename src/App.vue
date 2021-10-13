ca866481acf45b8574b25c56ae359c0a679cd653
Qwanjai Deelertpradit <d_khonjai@hotmail.com>
2021-10-13 23:02:30 +0700

    [:wrench:] Edit text slide layout

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,11 @@
 		>
 			<vueper-slide v-for="(slide, i) in slides" :key="i" :image="slide.image">
 				<template #content>
-					<div
-						class="vueperslide__content-wrapper"
-						style="flex-direction: column; color: white; margin-top: 10%; margin-right: 50%; "
-					>
-						<span>{{ slide.title }}</span>
-						<span>{{ slide.content }}</span>
+					<div class="slideText">
+						<span style="font-size:5vw; background-color: black;display: block; margin-bottom: 0.1em; padding: 2px;">{{
+							slide.title
+						}}</span>
+						<span style="font-size:2vw;  background-color: black;padding: 2px; ">{{ slide.content }}</span>
 					</div>
 				</template>
 			</vueper-slide>
@@ -28,16 +27,23 @@
 	margin-top: 8em;
 	margin-right: 20em;
 	font-size: 50px;
-}
-.vueperslide__content {
+}*/
+
+/* .vueperslide__content {
 	margin-right: 50em;
 	font-size: 25px;
 	color: greenyellow;
 } */
-/* .vueperslide__content-wrapper {
+.slideText {
+	bottom: 2em;
+	flex-direction: column;
+	margin-left: 1rem;
+	/* margin-top: 10rem; */
+	position: absolute;
+	left: 16px;
 	color: white;
-	font-size: 10px;
-} */
+	text-align: left;
+}
 </style>
 
 <script>
@@ -48,22 +54,37 @@ export default {
 	components: { VueperSlides, VueperSlide },
 	data: () => ({
 		breakpoints: {
+			// 1200: {
+			// 	slideRatio: 3 / 5,
+			// },
+			// 900: {
+			// 	slideRatio: 1 / 2,
+			// },
+			// 600: {
+			// 	slideRatio: 1 / 2,
+			// 	arrows: false,
+			// 	bulletsOutside: false,
+			// },
+			// 1500: {
+			// 	slideRatio: 3 / 5,
+			// },
+			// 300: {
+			// 	slideRatio: 1 / 2,
+			// },
 			1200: {
-				slideRatio: 3 / 5,
+				slideRatio: 1 / 5,
 			},
 			900: {
-				slideRatio: 1 / 2,
+				slideRatio: 1 / 3,
 			},
 			600: {
 				slideRatio: 1 / 2,
 				arrows: false,
-				bulletsOutside: false,
+				bulletsOutside: true,
 			},
-			1500: {
-				slideRatio: 3 / 5,
-			},
-			300: {
-				slideRatio: 1 / 2,
+			// The order you list breakpoints does not matter, Vueper Slides will sort them for you.
+			1100: {
+				slideRatio: 1 / 4,
 			},
 		},
 		slides: [

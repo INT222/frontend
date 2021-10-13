b9ee50622b64049bba4e311524c9534eb8a27095
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-10-13 13:06:50 +0700

    [ADD] FindListBlock component

---
+++ src/components/FindListBlock.vue
@@ -0,0 +1,5 @@
+<template>
+		<div class="w-full md:grid md:grid-cols-6">
+			<img src="../src/assets/BlackWidow.jpg" class="h-48 rounded-md md:h-60 md:rounded-lg md:mx-auto">
+		</div>
+</template>

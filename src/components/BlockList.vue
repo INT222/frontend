df8889f048be4e416b74bd5be58677ce2057d3da
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-10-13 13:48:21 +0700

    [EDIT] content space BlockList component

--- src/components/BlockList.vue
+++ src/components/BlockList.vue
@@ -1,12 +1,12 @@
 <template>
 	<div>
 		<div class="">
-			<div class="w-full grid grid-cols-5 md:grid-cols-1">
+			<div class="w-full grid grid-cols-5 md:grid-cols-1 my-3 bg-black">
 				<img
 					src="../src/assets/BlackWidow.jpg"
 					class="h-48 col-span-2 flex-none bg-cover rounded-md md:rounded-l md:object-center md:h-60 md:mx-auto"
 				/>
-				<div class="col-span-3 mt-3 mx-auto overflow-hidden ">
+				<div class="col-span-3 mt-3 mx-4 ">
 					<p class="text-white font-medium text-base mt-2 mb-4 tracking-wide md:text-center md:mb-2">The Tomorrow War</p>
 					<p class="text-gray-400 text-sm tracking-wider mb-3  md:text-center md:mb-2">July 2, 2021</p>
 					<p class="text-gray-400 text-sm tracking-wider mb-1 md:text-center">Action/Sci-fi ‧ 2h13m</p>

451f962256eb9ce8f5223fb7d86b76891132d7cc
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-10-13 12:47:35 +0700

    [EDIT] grid BlockList component

--- src/components/BlockList.vue
+++ src/components/BlockList.vue
@@ -1,18 +1,19 @@
 <template>
 	<div>
-		<div class="bg-purple-700 md:grid md:grid-cols-6">
+		<div class="">
 			<div class="w-full grid grid-cols-5 md:grid-cols-1">
 				<img
 					src="../src/assets/BlackWidow.jpg"
 					class="h-48 col-span-2 flex-none bg-cover rounded-md md:rounded-l md:object-center md:h-60 md:mx-auto"
 				/>
-				<div class="col-span-3 mt-3 mx-auto overflow-hidden mb-10 ">
+				<div class="col-span-3 mt-3 mx-auto overflow-hidden ">
 					<p class="text-white font-medium text-base mt-2 mb-4 tracking-wide md:text-center md:mb-2">The Tomorrow War</p>
 					<p class="text-gray-400 text-sm tracking-wider mb-3  md:text-center md:mb-2">July 2, 2021</p>
 					<p class="text-gray-400 text-sm tracking-wider mb-1 md:text-center">Action/Sci-fi ‧ 2h13m</p>
 					<!-- import component --"FavButton"-- in this Line -->
+					<!-- <FavButton></FavButton> -->
 				</div>
-				<hr class="w-80 mx-6 border border-gray-500 mb-3 md:hidden">
+				<hr class="w-80 mx-6 border border-gray-500 mb-3 mt-3 md:hidden">
 			</div>
 			
 		</div>

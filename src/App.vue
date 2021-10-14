37aa3ea099b6aa4598f0bf051be3ae01b4d250df
fah <supanan.c@mail.kmutt.ac.th>
2021-10-14 16:50:04 +0700

    [ADD] add component SubHamburger

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
 	<w-app>
-		<div id="nav">
-			<!-- <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> -->
+		<!-- <div id="nav">
+			<router-link to="/">Home</router-link>|
+			<router-link to="/about">About</router-link>
 			<div class="mx-auto bg-red-300">
 				<w-button bg-color="success">success</w-button>
 				<w-button bg-color="error">error</w-button>
@@ -13,30 +13,41 @@
 				<w-button bg-color="purple-light4">purple-light4</w-button>
 				<w-icon color="primary">material-icons star</w-icon>
 			</div>
+		</div>-->
+		<!-- <router-view /> -->
+		<div>
+			<w-button class="ma1" @click="showDrawer = 'left'" outline color="primary">Show left drawer</w-button>
+			<w-drawer :value="!!showDrawer" :[position]="true" @close="showDrawer = false">
+				<w-button
+					class="button--close"
+					@click="showDrawer = false"
+					sm
+					outline
+					round
+					absolute
+					color="primary"
+					icon="wi-cross"
+				></w-button>
+			</w-drawer>
 		</div>
-		<router-view />
 	</w-app>
 </template>
 
-<style lang="scss">
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-}
-
-#nav {
-	padding: 30px;
-
-	a {
-		font-weight: bold;
-		color: #2c3e50;
-
-		&.router-link-exact-active {
-			color: #42b983;
+<script>
+export default {
+	data() {
+		return {
+			showDrawer: false
+		}
+	},
+	computed: {
+		position() {
+			return this.showDrawer || 'right'
 		}
 	}
 }
+
+</script>
+
+<style lang="scss">
 </style>

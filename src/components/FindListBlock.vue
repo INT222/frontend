1d50e301df1f23d12efc813376aaa8421e5597fa
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-10-14 20:44:32 +0700

    [EDIT] responsive FindListBlock

--- src/components/FindListBlock.vue
+++ src/components/FindListBlock.vue
@@ -1,5 +1,5 @@
 <template>
-		<div class="w-full md:grid md:grid-cols-6">
-			<img src="../src/assets/BlackWidow.jpg" class="h-48 rounded-md md:h-60 md:rounded-lg md:mx-auto">
+		<div class="w-full ">
+			<img src="../src/assets/BlackWidow.jpg" class="h-48 rounded-md tb:h-52 md:h-60 md:rounded-lg md:mx-auto">
 		</div>
 </template>

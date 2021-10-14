3e6715fcf2b1ab7f6a0848cf141e28ae2766ee35
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-10-14 15:14:05 +0700

    [EDTI] font size and 3 responsive Banner component

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,79 +1,70 @@
 <template>
 	<w-app>
 		<vueper-slides
-			ref="myVueperSlides"
 			fade
 			:breakpoints="breakpoints"
 			:touchable="true"
-			:parallax="parallax"
 			autoplay
 			@autoplay-pause="internalAutoPlaying = false"
 			@autoplay-resume="internalAutoPlaying = true"
 		>
 			<vueper-slide
-				id="slide"
-				v-for="(slide, ) in slides"
-				:key="slide.id"
+				v-for="(slide, i) in slides"
+				:key="i"
 				:image="slide.image"
 				:title="slide.title"
 				:content="slide.content"
-				:style="'text-align:' + 'left'"
 			></vueper-slide>
 		</vueper-slides>
 	</w-app>
 </template>
 <style>
-#slide {
-	color: yellowgreen;
-	font-size: 100px;
-	font-weight: 900;
-	text-align: left;
-	
+.vueperslide__title {
+	color: white;
+	margin-top: 25%;
+	margin-left: -50%;
+	font-size: 6vw;
 }
-/* .vueper-slide{} */
+.vueperslide__content {
+	color:white;
+	opacity: 0.8;
+	margin-top:-1%;
+	margin-left: -50%;
+	font-size: 4vw;
+}
 </style>
 
 <script>
-import { VueperSlides, VueperSlide } from 'vueperslides'
-import 'vueperslides/dist/vueperslides.css'
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
 
 export default {
 	components: { VueperSlides, VueperSlide },
 	data: () => ({
 		breakpoints: {
-			1200: {
-				slideRatio: 3 / 5
-			},
-			900: {
-				slideRatio: 1 / 2
-			},
 			600: {
 				slideRatio: 1 / 2,
 				arrows: false,
-				bulletsOutside: false
+			},
+			1400:{
+				slideRatio: 2 / 5,
 			},
 			1500: {
-				slideRatio: 3 / 5
+				slideRatio: 3 / 5,
 			},
-			300: {
-				slideRatio: 1 / 2
-			}
 		},
 		slides: [
 			{
-				title: 'Jennie Kim, Korea',
-				content: 'Member of Black Pink',
-				// You can also provide a URL for the image.
-				image: require('@/assets/AboutTime.jpg')
+				title: "Jennie Kim, Korea",
+				content: "Member of Black Pink",
+				image: require("@/assets/Dune.jpg"),
 			},
 			{
-				title: 'Black widow',
-				content: 'Marvel studio',
-				// You can also provide a URL for the image.
-				image: require('@/assets/BlackWidow.jpg')
+				title: "Black widow",
+				content: "Marvel studio",
+				image: require("@/assets/BlackWidow.jpg"),
 			},
 		],
-		parallax: 1,
 	}),
-}
+};
 </script>

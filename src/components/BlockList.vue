24415f4eac38762f494f43c56b56eb74c74fe552
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-10-17 22:08:14 +0700

    [EDIT] BlockList component

--- src/components/BlockList.vue
+++ src/components/BlockList.vue
@@ -1,21 +1,24 @@
 <template>
-	<div>
-		<div class="">
-			<div class="w-full grid grid-cols-5 tb:grid-cols-1 md:grid-cols-1 my-3">
-				<img
-					src="../src/assets/BlackWidow.jpg"
-					class="h-48 col-span-2 flex-none bg-cover rounded-md tb:h-52 tb:rounded-md tb:mx-auto md:rounded-lg md:h-60 md:mx-auto"
-				/>
-				<div class="col-span-3 mt-3 mx-4 ">
-					<p class="text-white font-medium text-base mt-2 mb-4 tracking-wide tb:text-center tb:mb-2 md:text-center md:mb-2">The Tomorrow War</p>
-					<p class="text-gray-400 text-sm tracking-wider mb-3 tb:text-center tb:mb-2 md:text-center md:mb-2">July 2, 2021</p>
-					<p class="text-gray-400 text-sm tracking-wider mb-1 tb:text-center md:text-center">Action/Sci-fi ‧ 2h13m</p>
-					<!-- import component --"FavButton"-- in this Line -->
-					<!-- <FavButton></FavButton> -->
-				</div>
-				<hr class="w-80 mx-6 border border-gray-500 mb-3 mt-3 tb:hidden md:hidden">
+	<div class="">
+		<div class="w-full grid grid-cols-5 my-3 tb:w-44 tb:grid-cols-1 md:grid-cols-1">
+			<img
+				src="../src/assets/BlackWidow.jpg"
+				class="h-48 col-span-2 flex-none bg-cover rounded-md tb:h-52 tb:rounded-md tb:mx-auto md:rounded-lg md:h-60 md:mx-auto"
+			/>
+			<div class="col-span-3 mt-3 mx-4 tb:mx-auto md:mx-auto">
+				<p
+					class="text-white font-medium text-base mt-2 mb-4 tracking-wide tb:text-center tb:mb-2 md:text-center md:mb-2"
+				>The Tomorrow War</p>
+				<p
+					class="text-gray-400 text-sm tracking-wider mb-3 tb:text-center tb:mb-2 md:text-center md:mb-2"
+				>July 2, 2021</p>
+				<p
+					class="text-gray-400 text-sm tracking-wider mb-1 tb:text-center md:text-center"
+				>Action/Sci-fi ‧ 2h13m</p>
+				<!-- <FavButton></FavButton> -->
 			</div>
-			
+
+			<hr class="w-80 mx-6 border border-gray-500 mb-3 mt-3 tb:hidden md:hidden" />
 		</div>
 	</div>
 </template>

42200b7e910700d375c9cec2d3f8eac8a725f006
Qwanjai Deelertpradit <d_khonjai@hotmail.com>
2021-10-19 13:54:58 +0700

    [:package:] Add dropdown list

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
 	<w-app>
-		<div id="nav">
-			<!-- <router-link to="/">Home</router-link> |
+		<!-- <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> -->
-			<div class="mx-auto bg-red-300">
+		<!-- <div class="mx-auto bg-red-300">
 				<w-button bg-color="success">success</w-button>
 				<w-button bg-color="error">error</w-button>
 				<w-button bg-color="warning">warning</w-button>
@@ -12,31 +11,32 @@
 				<w-button bg-color="secondary">secondary</w-button>
 				<w-button bg-color="purple-light4">purple-light4</w-button>
 				<w-icon color="primary">material-icons star</w-icon>
+			</div> -->
+
+		<div class="w-1/4 mx-auto my-auto  h-2/4 p-10 relative inline-block">
+			<w-button class=" p-4  left-auto " @click="showDropDown = !showDropDown" bg-color="red" color="white">
+				Click me <w-icon color="white" v-if="showDropDown == false">material-icons expand_more</w-icon>
+				<w-icon color="white" v-if="showDropDown">material-icons expand_less</w-icon>
+			</w-button>
+			<div class=" absolute w-full " v-if="showDropDown">
+				<a href="#" class="block bg-red-50 hover:bg-red-200 ">1</a>
+				<a href="#" class="block bg-red-50 hover:bg-red-200 ">2</a>
+				<a href="#" class="block bg-red-50 hover:bg-red-200 ">3</a>
+				<a href="#" class="block bg-red-50 hover:bg-red-200 ">4</a>
 			</div>
+
+			<span class="m-3">{{ showDropDown }}</span>
 		</div>
-		<router-view />
+
+		<!-- <router-view /> -->
 	</w-app>
 </template>
 
-<style lang="scss">
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-}
-
-#nav {
-	padding: 30px;
-
-	a {
-		font-weight: bold;
-		color: #2c3e50;
-
-		&.router-link-exact-active {
-			color: #42b983;
-		}
-	}
-}
-</style>
+<script>
+export default {
+	data: () => ({
+		items: [{ label: "Item 1" }, { label: "Item 2" }, { label: "Item 3" }, { label: "Item 4" }],
+		showDropDown: false,
+	}),
+};
+</script>

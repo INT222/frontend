ffaf4045f63a202de9f9995e504f127ed4f9c6a4
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-10-29 23:30:01 +0700

    [EDIT] button WatchListBlock component

--- src/components/WatchListBlock.vue
+++ src/components/WatchListBlock.vue
@@ -5,7 +5,9 @@
 			class="h-48 col-span-2 flex-none bg-cover rounded-md tb:rounded-lg tb:object-center tb:h-52 tb:mx-auto md:rounded-lg md:object-center md:h-60 md:mx-auto"
 		/>
 		<div class="col-span-3 mt-3 mx-4 tb:text-center md:text-center">
-			<p class="text-white font-medium text-base mt-2 mb-3 tracking-wide tb:mb-2 md:mb-2">The Tomorrow War</p>
+			<p
+				class="text-white font-medium text-base mt-2 mb-3 tracking-wide tb:mb-2 md:mb-2"
+			>The Tomorrow War</p>
 			<div class="hidden tb:space-x-2 tb:inline-flex tb:space-x-2 md:inline-flex">
 				<p class="text-gray-400 text-xs tracking-wider mb-3 tb:mb-2 md:mb-2">July 2, 2021</p>
 				<div class="-mt-1">
@@ -14,23 +16,23 @@
 				</div>
 			</div>
 
-			<p class="text-gray-400 text-xs tracking-wider mb-3 tb:overflow-hidden md:overflow-hidden">
-				Action/Sci-fi ‧ 2h13m
-			</p>
+			<p
+				class="text-gray-400 text-xs tracking-wider mb-3 tb:overflow-hidden md:overflow-hidden"
+			>Action/Sci-fi ‧ 2h13m</p>
 			<div class="inline-flex space-x-2 tb:hidden md:hidden">
 				<w-icon lg color="amber-light1">material-icons star</w-icon>
 				<span class="text-white text-md font-medium my-auto">4.5</span>
 			</div>
-		</div>
-		<div class="mx-auto">
-			<button>
-				<div
-					class="tb:px-6 tb:py-1 tb:border tb:border-gray-300 tb:rounded-md tb:text-center tb:text-sm tb:text-gray-300 tb:inline-flex tb:space-x-1"
-				>
-					<w-icon lg>material-icons remove</w-icon>
-					<p class="my-auto">Remove</p>
-				</div>
-			</button>
+			<div class="mx-auto">
+				<button>
+					<div
+						class="inline-flex space-x-3 border px-6 py-1 border-gray-300 mt-5 rounded-md text-center text-sm text-gray-300 space-x-1"
+					>
+						<w-icon lg>material-icons remove</w-icon>
+						<p class="my-auto">Remove</p>
+					</div>
+				</button>
+			</div>
 		</div>
 	</div>
 </template>

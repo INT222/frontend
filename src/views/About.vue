b2491642a214ffba4cf72568c6201bf77d397a89
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-11-15 15:01:22 +0700

    [EDIT] footer and about us

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="my-10 mx-10">
+    <div class="md:flex bg-gray-100 z-10 rounded-xl p-8 md:p-0">
+      <div class="w-6 h-6 bg-red-300 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" />
+      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <h1 class="text-lg font-semibold">Supanan Chotteratat</h1>
+        <div class="font-medium">
+          <h2 class="text-gray-800">62130500122</h2>
+          <!-- <h3 class="text-gray-500">front-end</h3> -->
+        </div>
+      </div>
+    </div>
+    <!-- <div class="w-full h-60 bg-deepBlue z-10 rotate-3 transform rounded-lg"></div> -->
   </div>
 </template>

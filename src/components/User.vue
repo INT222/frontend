ec571c3e298b528298fdeb6c5a365b325b2febc5
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-11-16 00:10:42 +0700

    [EDIT] user.vue

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="bg-white">
-        <w-button @click="dialog.show = true">Open dialog</w-button>
+    <div class="bg-green-300">
+        <w-button @click="dialog.show = true" bg-color="transparent" color="black">Open dialog</w-button>
         <w-dialog
             v-model="dialog.show"
             :persistent="dialog.persistent"
@@ -9,14 +9,20 @@
             :width="dialog.width"
         >
             <div class="space-y-4">
-                    <div class="flex justify-center divide-x">
-                    <div class="bg-yellow-100 w-16 h-16 object-cover rounded-full p-1 mx-auto"></div>
-                    </div>
-                    
+                <div class="justify-center divide-x">
+                    <w-divider class color="grey-dark5">
+                        <div class="bg-yellow-200 w-20 h-20 object-cover rounded-full p-1 mx-auto">
+                            <img class="w-16 mx-auto" src="../assets/avatar.png">
+                        </div>
+                    </w-divider>
+                </div>
+
                 <p class="text-center">@Test</p>
                 <p class="text-center">firstname lastname</p>
                 <div class="flex justify-center pt-4">
-                    <w-button bg-color="purple-light3" color="white">Edit Profile</w-button>
+                    <router-link to="/editprofile">
+                        <w-button bg-color="purple-light1" color="white">Edit Profile</w-button>
+                    </router-link>
                 </div>
             </div>
         </w-dialog>

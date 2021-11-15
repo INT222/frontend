7bf845932247a10d140246377820e48265da7157
fah <supanan.c@mail.kmutt.ac.th>
2021-11-15 21:48:29 +0700

    [EDIT] edit user profile

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,57 +1,35 @@
 <template>
     <div class="bg-white">
-        <w-button class="px4" @click="dialog.show = true">Open dialog</w-button>
-
+        <w-button @click="dialog.show = true">Open dialog</w-button>
         <w-dialog
             v-model="dialog.show"
-            :fullscreen="dialog.fullscreen"
-            :width="dialog.width"
             :persistent="dialog.persistent"
             :persistent-no-animation="dialog.persistentNoAnimation"
             title-class="primary-light1--bg white"
+            :width="dialog.width"
         >
-            <template #title>
-                <w-icon class="mr2">mdi mdi-tune</w-icon>Control panel
-            </template>
-
-            <w-checkbox class="d-flex mt2" v-model="dialog.fullscreen" label="Fullscreen"></w-checkbox>
-            <w-checkbox
-                class="d-flex mt2"
-                v-model="dialog.persistent"
-                label="Persistent (try to click outside)"
-            ></w-checkbox>
-            <w-checkbox
-                class="d-flex mt2"
-                v-model="dialog.persistentNoAnimation"
-                :disabled="!dialog.persistent"
-                label="Persistent with no animation"
-            ></w-checkbox>
-
-            <div class="w-flex mt6 no-grow">
-                <span class="mr2">Width:</span>
-                <w-radios
-                    v-model="dialog.width"
-                    :items="[{ value: 300, label: '300px' }, { value: 500, label: '500px' }]"
-                    inline
-                />
+            <div class="space-y-4">
+                    <div class="flex justify-center divide-x">
+                    <div class="bg-yellow-100 w-16 h-16 object-cover rounded-full p-1 mx-auto"></div>
+                    </div>
+                    
+                <p class="text-center">@Test</p>
+                <p class="text-center">firstname lastname</p>
+                <div class="flex justify-center pt-4">
+                    <w-button bg-color="purple-light3" color="white">Edit Profile</w-button>
+                </div>
             </div>
-
-            <template #actions>
-                <div class="spacer" />
-                <w-button @click="dialog.show = false">Close</w-button>
-            </template>
         </w-dialog>
     </div>
 </template>
 
 <script>
 
-export default{
+export default {
     data() {
         return {
             dialog: {
                 show: false,
-                fullscreen: false,
                 persistent: false,
                 persistentNoAnimation: false,
                 width: 300

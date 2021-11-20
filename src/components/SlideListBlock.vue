740350d2bb0fce8ae19a25711570e2fabf0792ed
Qwanjai Deelertpradit <d_khonjai@hotmail.com>
2021-11-21 03:03:27 +0700

    [:wrench:] Add props

--- src/components/SlideListBlock.vue
+++ src/components/SlideListBlock.vue
@@ -1,10 +1,13 @@
 <template>
 	<div class="mx-auto">
 		<div class="w-32 tb:w-40 md:w-44">
-			<img src="../assets/BlackWidow.jpg" class="h-44 rounded-md mx-auto mb-2 tb:h-52 md:h-60" />
-			<p class="text-white text-center text-sm font-medium leading-snug overflow-hidden uppercase mb-2">{{ title }}</p>
+			<!-- <img src="../assets/Lucy.jpg" class="h-44 rounded-md mx-auto mb-2 tb:h-52 md:h-60" /> -->
+			<img :src="img" class="h-44 rounded-md mx-auto mb-2 tb:h-52 md:h-60" />
+			<p class="text-white text-center text-base font-bold leading-snug overflow-hidden uppercase mb-2 mt-2">
+				{{ moviename }}
+			</p>
 			<div class="mx-2 tb:mx-7 md:mx-9">
-				<w-rating md color="amber-light1"></w-rating>
+				<w-rating md color="amber-light1" :modelValue="avg_rating" readonly></w-rating>
 			</div>
 		</div>
 	</div>
@@ -12,9 +15,10 @@
 
 <script>
 export default {
-	props: [
-		"title"
-
-	]
-}
+	props: {
+		img: String,
+		moviename: String,
+		avg_rating: Number,
+	},
+};
 </script>

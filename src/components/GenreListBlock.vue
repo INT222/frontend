0cda2b2344f4d19fd77077943de50fef3987c0d0
fah <supanan.c@mail.kmutt.ac.th>
2021-11-20 15:54:14 +0700

    [EDIT] close dropdown when click outside

--- src/components/GenreListBlock.vue
+++ src/components/GenreListBlock.vue
@@ -1,36 +1,65 @@
 <template>
 	<div class="-ml-1 mt-1 md:mt-0">
 		<w-button
-            bg-color="transparent"
-            class="px-5 py-2 uppercase"
-            @click="showDropDown = !showDropDown"
-            color="white"
+			bg-color="transparent"
+			class="px-5 py-2 uppercase"
+			@click="showDropDown = !showDropDown"
+			color="white"
 			:height="40"
-        >
-
-		<div class="inline-flex space-x-3 -ml-4 md:px-1 md:my-0 ">
-			<div class="md:hidden">
-				<w-icon color="grey" :size="25">mdi mdi-filmstrip</w-icon>
+		>
+			<div class="inline-flex space-x-3 -ml-4 md:px-1 md:my-0">
+				<div class="md:hidden">
+					<w-icon color="grey" :size="25">mdi mdi-filmstrip</w-icon>
+				</div>
+				<p class="text-white my-auto md:my-2 text-sm uppercase">genre</p>
+				<div class="pl-2 pr-5 md:pr-1 md:pl-0 my-auto">
+					<w-icon md color="white" v-if="showDropDown == false">mdi mdi-chevron-down</w-icon>
+					<w-icon md color="white" v-if="showDropDown">mdi mdi-chevron-up</w-icon>
+				</div>
 			</div>
-			<p class="text-white my-auto md:my-2 text-sm uppercase">genre</p>
-			<div class="pl-2 pr-5 md:pr-1 md:pl-0 my-auto">
-				<w-icon md color="white" v-if="showDropDown == false">mdi mdi-chevron-down</w-icon>
-				<w-icon md color="white" v-if="showDropDown">mdi mdi-chevron-up</w-icon>
-			</div>
-		</div>
 		</w-button>
 		<div class="absolute mt-2 ml-14 md:w-28 md:ml-0 z-10 md:bg-blackBlue" v-if="showDropDown">
 			<div class="text-white text-xs uppercase font-medium">
-				<router-link to="/genre" class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6">action</router-link>
-				<a href="#" class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6">adventure</a>
-				<a href="#" class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6">comedy</a>
-				<a href="#" class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6">drama</a>
-				<a href="#" class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6">fantacy</a>
-				<a href="#" class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6">horror</a>
-				<a href="#" class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6">mystery</a>
-				<a href="#" class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6">romance</a>
-				<a href="#" class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6">sci-fi</a>
-				<a href="#" class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6">thriller</a>
+				<router-link @click.prevent="showDropDown = false"
+					to="/genre"
+					class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6"
+				>action</router-link>
+				<router-link @click.prevent="showDropDown = false"
+					to="/genre"
+					class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6"
+				>adventure</router-link>
+				<router-link @click.prevent="showDropDown = false"
+					to="/genre"
+					class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6"
+				>comedy</router-link>
+				<router-link @click.prevent="showDropDown = false"
+					to="/genre"
+					class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6"
+				>drama</router-link>
+				<router-link @click.prevent="showDropDown = false"
+					to="/genre"
+					class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6"
+				>fantasy</router-link>
+				<router-link @click.prevent="showDropDown = false"
+					to="/genre"
+					class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6"
+				>horror</router-link>
+				<router-link @click.prevent="showDropDown = false"
+					to="/genre"
+					class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6"
+				>mystery</router-link>
+				<router-link @click.prevent="showDropDown = false"
+					to="/genre"
+					class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6"
+				>romance</router-link>
+				<router-link @click.prevent="showDropDown = false"
+					to="/genre"
+					class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6"
+				>sci-fi</router-link>
+				<router-link @click.prevent="showDropDown = false"
+					to="/genre"
+					class="block hover:bg-gray-500 hover:bg-opacity-40 pr-4 py-2 md:pl-6"
+				>thriller</router-link>
 			</div>
 		</div>
 		<!-- <span class="m-3">{{ showDropDown }}</span> -->
@@ -43,6 +72,19 @@ export default {
 		items: [{ label: "Item 1" }, { label: "Item 2" }, { label: "Item 3" }, { label: "Item 4" }],
 		showDropDown: false,
 	}),
+	methods: {
+		documentClick(event) {
+			if (!this.$el.contains(event.target)) {
+				this.showDropDown = false;
+			}
+		},
+	},
+	created() {
+		document.addEventListener('click', this.documentClick)
+	},
+	unmounted() {
+		document.removeEventListener('click', this.documentClick)
+	}
 };
 </script>
 

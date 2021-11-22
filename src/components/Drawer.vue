4caed31778c9fe35d11ab53a5c648afd4b5bd5f8
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-11-23 00:13:34 +0700

    [EDIT] Drawer and EditProfile component

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -17,6 +17,15 @@
 				icon="wi-cross"
 			></w-button>
 			<div class="space-y-6 mx-5 mt-10 w-96">
+				<div class="space-y-1" v-if="this.loggedin === true">
+					<div class="flex text-sm space-x-2 font-medium ">
+						<p>Qwanjai</p>
+						<p>Deelertpradit</p>
+					</div>
+					<div class="text-xs text-gray-400 tracking-widest">
+						<p>@admin_qwan_edit</p>
+					</div>
+				</div>
 				<div v-if="this.loggedin === false">
 					<router-link to="/signin">
 						<w-button bg-color="transparent" class="text-sm text-white uppercase">sign in</w-button>
@@ -27,15 +36,20 @@
 						<w-button bg-color="transparent" class="text-sm text-white uppercase">sign up</w-button>
 					</router-link>
 				</div>
-				<div class="space-y-4" v-if="this.loggedin === true">
-					<user>Fah</user>
+				<div class="space-y-2 -ml-2" v-if="this.loggedin === true">
+					<router-link to="/editprofile">
+						<w-button
+							bg-color="transparent"
+							color="info-light1"
+							class="block uppercase text-xs w-auto tracking-wider"
+						>edit profile</w-button>
+					</router-link>
 					<w-button
 						@click="clickToLogOut"
-						bg-color="white"
-						color="black"
-						class="block uppercase text-xs hover:bg-gray-500 hover:bg-opacity-40 w-auto"
-						>log out</w-button
-					>
+						bg-color="transparent"
+						color="red"
+						class="block uppercase text-xs w-auto tracking-widest"
+					>sign out</w-button>
 				</div>
 				<w-divider class="w-36" color="grey" />
 				<div class="mt-6 -ml-2">
@@ -66,7 +80,7 @@
                             <button class="text-sm text-white uppercase">manage movie</button>
                         </router-link>
                     </div>
-                </div>-->
+				</div>-->
 			</div>
 		</w-drawer>
 	</div>
@@ -75,12 +89,12 @@
 <script>
 import GenreListBlock from "./GenreListBlock.vue";
 import { auth } from "../services/auth-module";
-import User from "./User.vue";
+// import User from "./User.vue";
 import authService from "../services/auth-service";
 export default {
 	components: {
 		"genre-list-block": GenreListBlock,
-		user: User,
+		// user: User,
 	},
 	data() {
 		return {

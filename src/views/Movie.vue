1b81bc37ba94dcfbd4cf986f3ae043eefba1822f
fah <supanan.c@mail.kmutt.ac.th>
2021-11-23 13:23:36 +0700

    [EDIT] detailblock

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="md:grid md:grid-rows-3 md:mx-auto md:w-9/12 md;h-auto">
+	<div class="md:grid md:grid-rows-3 md:w-9/12 md;h-auto">
 		<detail-block
 			:id="currentMovie.movie_id"
 			:name="currentMovie.moviename"
@@ -11,37 +11,26 @@
 			:trailer="currentMovie.trailer"
 		></detail-block>
 	</div>
-	<div class="space-y-4 mx-auto md:grid md:grid-rows-3 md:mx-auto md:w-auto md:h-auto">
-		<detail-block
-			:id="currentMovie.movie_id"
-			:name="currentMovie.moviename"
-			:releaseDate="currentMovie.releasedate"
-			:runtime="currentMovie.runtime"
-			:plot="currentMovie.plot"
-			:rating="currentMovie.avg_rating"
-			:imgPoster="getImage(currentMovie.poster)"
-			:trailer="currentMovie.trailer"
-		></detail-block>
-		<div class="bg-red-500">
+		<!-- <div class="bg-red-500">  -->
 			<!-- <comment-block
 				:username="currentMovie.comments[0].username"
 				:commentDate="currentMovie.comments[0].create_date"
 				:commentText="currentMovie.comments[0].commenttext"
 				:rating="currentMovie.comments[0].rating"
 			></comment-block> -->
-		</div>
-		<div class="bg-red-500">
+		<!-- </div> -->
+		<!-- <div class="bg-red-500"> -->
 			<!-- <comment-block
 				:username="currentMovie.comments[0].username"
 				:commentDate="currentMovie.comments[0].create_date"
 				:commentText="currentMovie.comments[0].commenttext"
 				:rating="currentMovie.comments[0].rating"
 			></comment-block> -->
-		</div>
+		<!-- </div> -->
 		<!-- <div class="text-white">
 			{{ currentMovie.comments }}
 		</div> -->
-	</div>
+	<!-- </div> -->
 </template>
 
 <script>

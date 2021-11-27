c09251fb295d144d789ad037a06f31e9332e8902
fah <supanan.c@mail.kmutt.ac.th>
2021-11-27 20:36:15 +0700

    [:lipstick:] edit responsive in Home page

--- src/components/SlideListBlock.vue
+++ src/components/SlideListBlock.vue
@@ -1,17 +1,26 @@
 <template>
-	<div class="mx-auto inline-flex md:space-x-28 tb:space-x-10 mb-3">
-		<div class="w-32 tb:w-40 md:w-44" v-for="s in slides" :key="s.movie_id">
-			<button @click="goRoute(s.movie_id)">
-			<img :src="getImage(s.poster)" class="h-44 rounded-md mx-auto mb-2 tb:h-52 md:h-60" />
-			<p class="text-white text-center text-base font-bold leading-snug overflow-hidden truncate w-28 md:w-44 tb:w-44 uppercase mb-2 mt-2">
-				{{ s.moviename }}
-			</p>
-			<div class="mx-2 tb:mx-7 md:mx-9">
-				<w-rating md color="amber-light1" :modelValue="s.avg_rating" readonly></w-rating>
+	<!-- <div class="mx-auto inline-flex md:space-x-28 tb:space-x-12 mb-3"> -->
+		<div class="flex overflow-x-scroll pb-10 hide-scroll-bar md:pb-0 md:flex-none md:overflow-x-hidden">
+			<div class="flex flex-nowrap md:flex-none">
+				<div class="inline-flex md:space-x-24 tb:space-x-12 md:flex-none">
+					<div class="w-32 mx-auto tb:w-40 md:w-44" v-for="s in slides" :key="s.movie_id">
+						<button @click="goRoute(s.movie_id)">
+							<img
+								:src="getImage(s.poster)"
+								class="max-w-xs overflow-hidden shadow-md hover:shadow-xl transition-shadow duration-300 ease-in-out h-44 rounded-md mb-2 tb:h-52 md:h-60"
+							/>
+							<p
+								class="text-white text-center text-base font-medium leading-snug overflow-hidden truncate tb:truncate tb:overflow-hidden w-32 md:w-40 tb:w-36 uppercase mb-2 mt-2"
+							>{{ s.moviename }}</p>
+							<div class="mx-2 tb:mx-7 md:mx-9">
+								<w-rating xs color="amber-light1" :modelValue="s.avg_rating" readonly></w-rating>
+							</div>
+						</button>
+					</div>
+				</div>
 			</div>
-			</button>
 		</div>
-	</div>
+	<!-- </div> -->
 </template>
 
 <script>
@@ -45,3 +54,12 @@ export default {
 	},
 };
 </script>
+<style scoped>
+.hide-scroll-bar {
+	-ms-overflow-style: none;
+	scrollbar-width: none;
+}
+.hide-scroll-bar::-webkit-scrollbar {
+	display: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
 		<p
 			class="text-white uppercase text-xl tb:text-2xl md:text-3xl font-medium md:mx-6 tb:mx-4 mx-2"
 		>popular in this month</p>
-		<div class="inline-flex md:space-x-28 tb:space-x-10 w-full mb-3">
+		<div class="md:mx-24 tb:ml-7 w-full mb-3">
 			<slide-list-block></slide-list-block>
 		</div>
 		<p

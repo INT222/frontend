8aeee1ddd83523ebdf6bec007497f01f51031b32
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-11-28 17:41:56 +0700

    [EDIT] comment component

--- src/components/CommentBlock.vue
+++ src/components/CommentBlock.vue
@@ -1,9 +1,9 @@
 <template>
-	<div class="mx-5 tb:mx-8 text-white md:grid md:grid-col-4 md:px-7 md:mx-12 space-y-5">
+	<div class="mx-5 tb:mx-8 text-white md:grid md:grid-col-4 md:mx-12 space-y-5">
 		<div v-for="c in comments" :key="c.commentid" class="md:grid md:grid-flow-col md:justify-self-start md:space-x-10">
 			<div class="md:text-center md:grid md:grid-row-2 fle md:justify-start">
 				<div>
-					<p class="font-medium mx-4 tracking-widest">{{ c.username }}</p>
+					<p class="font-medium mx-4 md:text-lg tracking-widest">{{ c.username }}</p>
 				</div>
 				<div class="inline-flex md:flex-col">
 					<div>
@@ -15,7 +15,7 @@
 				</div>
 			</div>
 			<div>
-				<p class="my-2 text-sm md:my-0 tracking-wide">
+				<p class="my-2 text-sm md:text-base md:my-0 tracking-wide">
 					{{ c.commenttext }}
 				</p>
 			</div>

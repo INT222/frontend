0155c7b571d8d02654ac9f9c0f6b0493eea56fac
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-12-04 12:14:52 +0700

    [EDIT] ExploreListBlock

--- src/components/ExploreListBlock.vue
+++ src/components/ExploreListBlock.vue
@@ -1,27 +1,21 @@
 <template>
-	<div class="space-y-5 md:flex tb:grid tb:grid-cols-3 tb:space-y-0 tb:space-x-4 md:gird md:grid-cols-3 md:grid-rows-2 md:space-y-0 md:space-x-9">
+	<div
+		class="space-y-5 md:flex tb:grid tb:grid-cols-3 tb:space-y-0 tb:space-x-4 md:gird md:grid-cols-3 md:grid-rows-2 md:space-y-0 md:space-x-9"
+	>
 		<!--Box 1-->
-		<div
-			v-for="s in slides"
-			:key="s.movie_id"
-			class=" rounded-md overflow-hidden shadow-lg"
-		>
-
-			<img
-				:src="getImage(s.poster)"
-				class="w-full h-48 object-cover"
-			/>
-			<div class="px-6 py-4 bg-gray-700">
-				<div class="font-bold text-xl mb-2 text-white">{{ s.title }}</div>
-				<p id="plot" class="text-white text-base md:w-96">{{ s.plot }}</p>
-				<div>
-					<button @click="goRoute(s.movie_id)">
-					<span
-						class="pt-2 text-sm font-medium flex justify-end text-deepBlue tb:pt-1 tb:text-base md:focus:text-blue-500"
-					>Read More</span>
-					</button>
+		<div v-for="s in slides" :key="s.movie_id" class="rounded-md overflow-hidden shadow-lg">
+			<button @click="goRoute(s.movie_id)">
+				<img :src="getImage(s.poster)" class="w-full h-48 object-cover" />
+				<div class="px-6 py-4 bg-gray-700">
+					<div class="font-bold text-xl mb-2 text-white">{{ s.title }}</div>
+					<p id="plot" class="text-white text-base text-left md:w-96">{{ s.plot }}</p>
+					<div>
+						<span
+							class="pt-2 text-sm font-medium flex justify-end text-deepBlue tb:pt-1 tb:text-base md:focus:text-blue-500"
+						>Read More</span>
+					</div>
 				</div>
-			</div>
+			</button>
 		</div>
 	</div>
 </template>

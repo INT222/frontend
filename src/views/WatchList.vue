af282fc339f48bd15a9f6059b8f5d3319b198397
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-12-05 21:59:53 +0700

    [EDIT] watchList component

--- src/views/WatchList.vue
+++ src/views/WatchList.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="mx-5 tb:mt-5">
         <h1 class="uppercase text-white text-xl font-semibold ml-2 mb-6 md:ml-9">my watch list</h1>
-        <watch-list-block></watch-list-block>
+        <button @click="goRoute(m.movie_id)">
+            <watch-list-block></watch-list-block>
+        </button>
     </div>
 </template>
 
@@ -10,6 +12,11 @@ import WatchListBlock from "@/components/WatchListBlock.vue";
 export default {
     components: {
         'watch-list-block': WatchListBlock,
+    },
+    methods: {
+        goRoute(movieId) {
+            this.$router.push(`/movie/${movieId}`);
+        },
     }
 }
 </script>

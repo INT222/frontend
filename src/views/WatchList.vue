f109b0011e49c0bffe85b7bafb9e39576f8c9a43
fah <supanan.c@mail.kmutt.ac.th>
2021-12-06 19:02:24 +0700

    [:wrench:] change click method

--- src/views/WatchList.vue
+++ src/views/WatchList.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="mx-5 tb:mt-5">
         <h1 class="uppercase text-white text-xl font-semibold ml-2 mb-6 md:ml-9">my watch list</h1>
-        <button @click="goRoute(m.movie_id)">
             <watch-list-block></watch-list-block>
-        </button>
     </div>
 </template>
 
@@ -13,10 +11,5 @@ export default {
     components: {
         'watch-list-block': WatchListBlock,
     },
-    methods: {
-        goRoute(movieId) {
-            this.$router.push(`/movie/${movieId}`);
-        },
-    }
 }
 </script>
